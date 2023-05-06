7310ca55c13ff8560df739da9a962b8b19b746a7
GERLAN PEREIRA MAGALHAES <111335734+gerlan06@users.noreply.github.com>
2023-05-06 18:28:48 -0300

    new
    new

---
+++ script.js
@@ -0,0 +1,17 @@
+window. sr = ScrollReveal ({reset: true})
+
+
+sr.reveal('.caixa h3', {
+    rotate:{x:0, y:80, z:0}, duration:2000 });
+
+    sr.reveal('.caixa-container', {
+        rotate:{x:0, y:80, z:0}, duration:2000 });
+    
+
+    sr.reveal('.caixa-container img', {
+        rotate:{x:0, y:80, z:0}, duration:3000 });
+    
+
+    sr.reveal('.caixa p', {
+        rotate:{x:0, y:80, z:0}, duration:2500 });
+
